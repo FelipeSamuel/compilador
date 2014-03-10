4449f6331b812236836ebd0b26bfd3d2e5789681
juan <juan.corrales@cerok.com>
2014-03-10 12:40:28 -0500

    Se crean los lexemas para string, numero, hexagecimal, comentarios y operadires unario, binarios y de agrupacion con su respectivo manejo de errores

--- lexer.py
+++ lexer.py
@@ -5,7 +5,7 @@
 # numbers and +,-,*,/
 # ------------------------------------------------------------
 import ply.lex as lex
-
+from ply.lex import TOKEN
 '''
 Diccionario de datos que contiene la lista de las
 palabras reservadas
@@ -41,43 +41,220 @@ datos que contiene las palabras reservadas
 @var token lista
 '''
 tokens = [
+            'LNR',
+            'NUMBER',
+            'ENUMBER',
+            'NUMBEREX',
             'ID',
             'NID',
             'BCOMMENT',
             'LCOMMENT',
             'ECOMMENT',
-            'NUMBER'
+            'CSTRING',
+            'ECSTRING',
+            'PLUS',
+            'MINUS',
+            'TIMES',
+            'DIVIDE',
+            'MODULE',
+            'ME'
+            'LPAREN',
+            'RPAREN'
          ] + list(reserved.values())
+
+'''
+Lexema para LINEAS NO RECONOCIDAS
+se crea por la necesidad de comentar dentro
+del codigo fuente y que no aparesca en la consola
+
+@function t_LNR
+@param  t (instancia del simbolo)
+@return t (instancia del simbolo)
+'''
+def t_LNR(t):
+    r'\#\#.*'
+    #print t.value
+    return ''
 
 
 '''
+Apartir de esta linea se empieza a realizar la reglas(expresiones regulares)
+que definen cada toke la cuales se llamaran lexemas.
+
+Como estrategia se implementaran variables que contienen expresiones regulares
+las cuales se contatenaran para cada caso, esto con el fin de poder hacer un
+mejor seguimiento al codigo en caso de presentarse errores.
+
+
+Conjunto de lexemas para reconocer numeros validos
+
+Lexema que define el numero, debe reconocer lo siguiente
+  233232
+  54545
+  04344
+  0
+'''
+numero = r'[1-9]\d+|0[1-9]\d+|0'
+
+'''
+Lexema que define el numero con signo, debe reconocer lo siguiente
+  -232
+  -44
+'''
+numero_signo = r'\-'+numero+r''
+
+'''
+Lexema que define el numero con signo y punto flotante, debe reconocer lo siguiente
+  454.767
+  -290.8989
+  -.44
+  -0.4646
+'''
+numero_punto_flotante = r'\-?([1-9]\d+|0[1-9]\d+|0)\.\d+|\-?\.\d+'
+
+'''
+Lexema que define el numero con signo, punto flotante y exponencial, debe reconocer lo siguiente
+  454.767
+  -290.8989
+  -.44
+  -0.4646
+'''
+
+numero_exponencial = r'\-?\d+E[\-\+]?\d+|\-?\d+\.\d+E[\-\+]?\d+|\-?\.\d+E[\-\+]?\d+'
+
+'''
+Union de todos los lexemas que definen un numero
+'''
+number = r''+numero_exponencial+r'|'+numero_punto_flotante+r'|'+numero_signo+r'|'+numero+r''
+
+
+
+'''
+Conjunto de Lexemas para reconocer numeros no validos
+
+Lexema que reconoce numeros que tengan 2 o mas ceros a la izquierda, debe reconocer lo
+siguiente:
+
+  00000000
+  -000000
+  000004545
+  -000056
+'''
+no_ceros    = r'\-?(0{2,})\d+(\.\d*)?'
+
+'''
+Lexema que reconoce numeros que tengan 2 o mas signos negativos, debe reconocer lo
+siguiente:
+
+  -----000000
+  ---56
+  ---000056
+  0---556
+  00000---556
+  000565600---55600
+  000565600---00000--55600
+'''
+no_minus    = r'(\-{2,}\d+{\-2,}\d+)+|(\d+\-{2,}\d+)+|(\d+\-{2,})+|(\-{2,}\d+)+'
+
+'''
+Lexema que reconoce numeros que tengan 2 o mas puntos flotantes, debe reconocer lo
+siguiente:
+
+  ....32
+  -...9776
+  ----.78
+  ----....90
+  ----....000090
+  545....5454
+  676.65656.66565
+  65656.
+  87878.....
+'''
+no_punto = r'(\-{2,}|\-)?(\.{2,}\d+|\d+(\.{2,}\d+)+|\d+\.{2,})|\-{2,}\.\d+|\-\.{2,}\d+|\d+(\.+\d+\.+\d+)+'
+
+'''
+Lexema que reconoce numeros que tengan 2 o mas exponenciales, debe reconocer lo
+siguiente:
+
+  536-E
+  5878-EEE
+  58899---E
+  58000---EEE
+  -23443-E
+  5878-EEE
+  58899---E
+  58000---EEE
+  00000078-E
+  5878-EEE
+  58899---E
+  58000---EEE
+  ---3434-E
+  ---00007575-E
+'''
+no_exponencial = r'\-*\d+(\.+\d+)*\-+E+([\+\-]*\d*\.*\d*)+'
+
+nonumber = r''+no_exponencial+r'|'+no_punto+r'|'+no_ceros+r'|'+no_minus+r''
+
+
+'''
+Lexema para reconocer numeros en formato hexaesimal.
+
+@function t_NUMBEREX
+@param  t (instancia del simbolo)
+@return t (instancia del simbolo)
+'''
+
+
+def t_NUMBEREX(t):
+    r'0x[0-9A-Fa-f]+'
+    return t
+
+
+'''
+Lexema para reconocer numeros no validos.
+
+@function t_ENUMBER
+@param  t (instancia del simbolo)
+@return t (instancia del simbolo)
+'''
+@TOKEN(nonumber)
+
+def t_ENUMBER(t):
+    print "ERROR: Numero '%s' invalido en la linea %d columna %d" % (t.value, t.lexer.lineno, obtener_columna(t.lexer.lexdata, t))
+t_ENUMBER.__doc__ = nonumber
+
+'''
 Lexema para reconocer numeros.
+
 @function t_NUMBER
 @param  t (instancia del simbolo)
 @return t (instancia del simbolo)
 '''
+
+@TOKEN(number)
+
 def t_NUMBER(t):
-    r'[+-]?(\d*\.\d+|\d+\.\d*)([Ee][+-]?\d+)? | (\d+[Ee][+-]?\d+)'    
-    #r'[+-]?((0|[1-9][0-9]*)\.\d+)'    
     return t
+t_NUMBER.__doc__ = number
 
 '''
 Lexema para reconocer identificadores no validos.
 Hace las siguientes validaciones:
   * Si existe una cadena de entrada que empieza con
-    numeros saca el error de identificador invalido    
+    numeros saca el error de identificador invalido
 @function t_NID
 @param  t (instancia del simbolo)
 @return t (instancia del simbolo)
 '''
 def t_NID(t):
-    r'[0-9][a-zA-Z_0-9\-\_]*'
-    print "ERROR: Identificador '%s' invalido en la linea %d columna %d" % (t.value, t.lexer.lineno, obtener_columna(t.lexer.lexdata, t))        
+    r'\d+[a-zA-Z_\-_]+[0-9\-_]*'
+    print "ERROR: Identificador '%s' invalido en la linea %d columna %d" % (t.value, t.lexer.lineno, obtener_columna(t.lexer.lexdata, t))
+
 
 '''
 Lexema para reconocer identificadores.
 Hace las siguientes validaciones:
-  * Que le identificador no se encuentre
+  * Que el identificador no se encuentre
     entre las palabras reservadas
   * Si la longitud del identificador tiene
     mas de 20 caracteres devuelve los primeros
@@ -90,8 +267,32 @@ def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9\-\_]*'
     t.type = reserved.get(t.value,'ID')
     if len(t.value) > 20:
-      t.value = t.value[0:20]    
+      t.value = t.value[0:20]
     return t
+
+'''
+Lexema para reconocer un string
+debe reconocer las siguientes expresiones
+
+  "esto es una cadena de caracteres"
+  "esto es una cadena \" escapada\" "
+'''
+def t_CSTRING(t):
+    r'"{1}([^\\"]|\\(.|\n))*"{1}'
+    t.lexer.lineno += t.value.count("\n")
+    return t
+
+'''
+Lexema para reconocer un string invalido
+debe reconocer las siguientes expresiones
+
+  "esto es una cadena de caracteres
+  "esto es una cadena \" escapada\"
+'''
+def t_ECSTRING(t):
+    r'"([^\\"]|\\(.|\n))*|"{2,}'
+    print "FATAL ERROR: La cadena de caracteres '%s' no esta cerrada linea %d columna %d" % (t.value, t.lexer.lineno, obtener_columna(t.lexer.lexdata, t))
+    exit(0)
 
 '''
 Lexema para reconocer lineas comentadas
@@ -100,7 +301,7 @@ Lexema para reconocer lineas comentadas
 @return t (instancia del simbolo)
 '''
 def t_LCOMMENT(t):
-    r'//(.)*'    
+    r'//(.)*'
     return t
 
 '''
@@ -115,37 +316,49 @@ def t_BCOMMENT(t):
     return t
 
 '''
-Lexema para reconocer error del no cierre del bloque 
+Lexema para reconocer error del no cierre del bloque
 comentarios
 @function t_ECOMMENT
 @param  t (instancia del simbolo)
 @return t (instancia del simbolo)
 '''
 def t_ECOMMENT(t):
-    r'/\*(.|\n|\r|\t)*?'    
-    print "FATAL ERROR: El bloque de comentario '%s' no esta cerrado linea %d columna %d" % (t.value, t.lexer.lineno, obtener_columna(t.lexer.lexdata, t))        
+    r'/\*(.|\n|\r|\t)*?'
+    print "FATAL ERROR: El bloque de comentario '%s' no esta cerrado linea %d columna %d" % (t.value, t.lexer.lineno, obtener_columna(t.lexer.lexdata, t))
     exit(0)
 
 '''
-list(reserved.values())
-# List of token names.   This is always required
-tokens = (   
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
-)
-
-# Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+Lexemas para operadores unarios, binarios, aritmeticos y de agrupacion
 '''
+
+def t_PLUS(t):
+  r'\+'
+  return t
+
+def t_MINUS(t):
+  r'-'
+  return t
+
+def t_TIMES(t):
+  r'\*'
+  return t
+
+def t_DIVIDE(t):
+  r'/'
+  return t
+
+def t_MODULE(t):
+  r'%'
+  return t
+
+def t_LPAREN(t):
+  r'\('
+  return t
+
+def t_RPAREN(t):
+  r'\)'
+  return t
+
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -182,7 +395,7 @@ def leer_archivo():
     cadenas de caracteres de cada linea en la intrada
     del analizador lexico
     '''
-    with open('archivos_prueba/numeros.txt') as linea:
+    with open('archivos_prueba/tokens_simples.txt') as linea:
         datos = linea.read()
         lexer.input(datos)
     '''
